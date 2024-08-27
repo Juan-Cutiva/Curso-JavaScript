b1f468ac62a8d10f6e5ad2e8272cf343418344bb
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-27 18:01:02 -0500

    "Refactored conditional logic in main.js, added new function cuadrado(), and updated template string in document.querySelector"

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,11 +12,27 @@ function i(min, max) {
 var c = i(0, 3);
 console.log(c);
 
-const resultado = c === 0 ? 'salio 0' : '' || c === 1 ? 'salio 1' : '' || c === 2 ? 'salio 2' : '';
+const resultado = c === c ? `salio ${c}` : 'Error' 
+
+
+function cuadrado(){
+	if(c === 0){
+		const r ='banano'
+		return r
+	}else if (c === 1){	
+		const r = 'manzana'
+		return r
+	}else {
+		const r = 'pera'
+		return r
+	}
+}
+console.log(cuadrado())
+
 
 document.querySelector('#prueba').innerHTML = `
 <h1 class="text-red-900 text-7xl font-extrabold">Prueba</h1>
-<h1 class="text-red-900 text-7xl font-extrabold">${carro.marca + ' el cual tiene este modelo ' + carro.modelo + ' de un color ' + carro.color + 'en el cual se puede consumir esta fruta ' + frutas[2] + ` hola ${frutas[0]} ${resultado}`}</h1>
+<h1 class="text-red-900 text-7xl font-extrabold">${carro.marca + ' el cual tiene este modelo ' + carro.modelo + ' de un color ' + carro.color + 'en el cual se puede consumir esta fruta ' + frutas[2] + ` hola ${frutas[0]} ${resultado} ${cuadrado()}`}</h1>
 `;
 
 console.log('prueba');
