a273cc68907a1fad4498730f8cfc9dc9a5a10680
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-29 12:46:24 -0500

    [FINISH]

--- src/notas/juego.js
+++ src/notas/juego.js
@@ -5,11 +5,17 @@ var numeroSecreto = i(0, 11);
 console.log(numeroSecreto);
 
 var botonEnviar = document.getElementById('enviar');
+var botonReiniciar = document.getElementById('reiniciar')
 
 botonEnviar.addEventListener('click', recibir);
 botonEnviar.addEventListener('click', resultado);
 botonEnviar.addEventListener('click', h);
 botonEnviar.addEventListener('click', numeroJugadorResultado);
+botonReiniciar.addEventListener('click', reiniciar)
+
+function reiniciar(){
+	location.reload()
+}
 
 function recibir() {
 	var numeroJugador = document.getElementById('texto').value;
@@ -45,16 +51,20 @@ function numeroJugadorResultado() {
 		const r = 'El numero que ingreso el jugador es incorrecto';
 		return r;
 	} else if (resultado() === 'Ganaste' || resultado() === 'Perdiste') {
-		const r = `El numero Jugador es ${recibir()}`;
+		const r = `El numero Jugador del es ${recibir()}`;
 		return r;
 	}
 }
 
+
 function h() {
+	const color = resultado() == "Ganaste" ? 'bg-green-500' : 'bg-red-500'
+	botonReiniciar.style.display="flex"
+	botonReiniciar.style.justifyContent="center"
 	console.log(resultado());
 	console.log(numeroJugadorResultado());
-	document.querySelector('#prueba').innerHTML = `
+	document.querySelector('#result').innerHTML = `
 <h1>El numero secreto es ${numeroSecreto}</h1>
 <h1>${numeroJugadorResultado()}</h1>
-<h1>${resultado()}</h1>`;
+<h1 class='w-[30%] rounded-lg ${color}'>${resultado()}</h1>`;
 }
