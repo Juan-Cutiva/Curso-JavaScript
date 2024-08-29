8f8a238804937e679384d1e471cc8e8bda703f9f
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-29 10:40:45 -0500

    "Updated settings.json, juego.js, and index.astro files with various code changes"

--- src/notas/juego.js
+++ src/notas/juego.js
@@ -2,29 +2,31 @@ function i(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
 var numeroSecreto = i(0, 11);
-
-const numeroJugador = parseInt(prompt('Adivina el numero secreto entre 0 y  10'));
-console.log(numeroJugador);
+console.log(numeroSecreto);
 
 var botonEnviar = document.getElementById('enviar');
 
 botonEnviar.addEventListener('click', recibir);
+botonEnviar.addEventListener('click', resultado);
+botonEnviar.addEventListener('click', h);
+botonEnviar.addEventListener('click', numeroJugadorResultado);
 
 function recibir() {
-    var text = document.getElementById('texto').value
-    result = text
+	var numeroJugador = document.getElementById('texto').value;
+	const r = numeroJugador;
+	return Number(r);
 }
 
 function resultado() {
-	if (numeroJugador >= '0' && numeroJugador <= '10') {
-		if (numeroJugador === numeroSecreto) {
+	if (recibir() >= 0 && recibir() <= 10) {
+		if (recibir() == numeroSecreto) {
 			const r = 'Ganaste';
 			return r;
 		} else {
 			const r = 'Perdiste';
 			return r;
 		}
-	} else if (numeroJugador <= '0' || numeroJugador >= '10') {
+	} else if (recibir() <= 0 || recibir() >= 10) {
 		const r = 'Solo se pueden ingresar numero entre el 0 y el 10, vuelve a intentar, Perdiste';
 		return r;
 	} else {
@@ -43,13 +45,16 @@ function numeroJugadorResultado() {
 		const r = 'El numero que ingreso el jugador es incorrecto';
 		return r;
 	} else if (resultado() === 'Ganaste' || resultado() === 'Perdiste') {
-		const r = `El numero Jugador es ${numeroJugador}`;
+		const r = `El numero Jugador es ${recibir()}`;
 		return r;
 	}
 }
-document.querySelector('#prueba').innerHTML = `
+
+function h() {
+	console.log(resultado());
+	console.log(numeroJugadorResultado());
+	document.querySelector('#prueba').innerHTML = `
 <h1>El numero secreto es ${numeroSecreto}</h1>
 <h1>${numeroJugadorResultado()}</h1>
 <h1>${resultado()}</h1>`;
-
-
+}
