c22c2e0758b6e0cfbe5eb37e05d8691348772093
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-30 16:25:35 -0500

    Ciclos For

--- src/notas/bucles/for.js
+++ src/notas/bucles/for.js
@@ -1,4 +1,41 @@
 let list = ['eat', 'sleep', 'code', 'repeat'];
-for(let i = 0; i < list.length; i++){
-    console.log(list[i]);
+for (let i = 0; i < list.length; i++) {
+	console.log(list[i]);
 }
+
+// For Of
+// For Of sirve para arrays y strings
+
+let canasta = ['manzana', 'pera', 'naranja', 'uva', 'sandia'];
+console.log(canasta);
+for (fruta of canasta) {
+	console.log(fruta);
+}
+
+
+// For In  ---> para objetos numerables
+// Propiedades = valor
+
+const listaDeCompras = {
+    manzana : 5,
+    pera : 3,
+    naranja : 2,
+    uva : 1,
+    mango: 1 
+}
+var fruta
+for (fruta in listaDeCompras){
+    console.log(fruta);
+}
+
+// En resumen para Array y String se usa el For Of
+// Para Objetos se usa el For In
+
+// Array = let canasta = ['manzana', 'pera', 'naranja', 'uva', 'sandia'];
+// Objeto = const listaDeCompras = {
+//             manzana : 5,
+//             pera : 3,
+//             naranja : 2,
+//             uva : 1,
+//             mango: 1 
+//             }  
