698e11fb647b3a6b73a2fae478a692ce34981891
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-08-30 17:49:18 -0500

    While

--- src/notas/bucles/while.js
+++ src/notas/bucles/while.js
@@ -3,3 +3,11 @@ while(contador < 10){
     console.log(contador)
     contador++
 }
+
+// Do While 
+// do...while loop: Similar al while loop, pero se ejecuta al menos una vez. Osea si o si se va a ejecutar
+// el código una vez y ya después valida el while
+do{
+    console.log(contador);
+    contador++;
+}while (contador < 10);
