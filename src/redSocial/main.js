1bccbbdcfe785b34ce2bb8484536a677b1eee8f7
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-12 18:43:32 -0500

    "Removed hardcoded user data and imported from separate file"

--- src/redSocial/main.js
+++ src/redSocial/main.js
@@ -1,14 +1,4 @@
-const usersDatabase = [
-	{ username: 'andres', password: 123, gender: 'male' },
-	{ username: 'caro', password: 456, gender: 'female' },
-	{ username: 'mariana', password: 789, gender: 'female' }
-];
-const usersTimeline = [
-	{ username: 'Estefany', timeline: 'Me encanta Javascript!' },
-	{ username: 'Oscar', timeline: 'Developer es lo mejor!' },
-	{ username: 'Mariana', timeline: 'A mi me gusta más el café que el té' },
-	{ username: 'Andres', timeline: 'Yo hoy no quiero trabajar' }
-];
+import { usersDatabase, usersTimeline } from './usersDatabase';
 
 var botonEnviar = document.getElementById('botonEnviar');
 botonEnviar.addEventListener('click', ejecutar);
---
+++ src/redSocial/usersDatabase.js
@@ -0,0 +1,11 @@
+export const usersDatabase = [
+	{ username: 'andres', password: 123, gender: 'male' },
+	{ username: 'caro', password: 456, gender: 'female' },
+	{ username: 'mariana', password: 789, gender: 'female' }
+];
+export const usersTimeline = [
+	{ username: 'Estefany', timeline: 'Me encanta Javascript!' },
+	{ username: 'Oscar', timeline: 'Developer es lo mejor!' },
+	{ username: 'Mariana', timeline: 'A mi me gusta más el café que el té' },
+	{ username: 'Andres', timeline: 'Yo hoy no quiero trabajar' }
+];
