2d8bb968fc230170a6eb162ba82395dd449f2604
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-12 12:44:42 -0500

    maquetación login

---
+++ src/redSocial/main.js
@@ -0,0 +1,47 @@
+const usersDatabase = [
+	{ username: 'andres', password: '123' },
+	{ username: 'caro', password: '456' },
+	{ username: 'mariana', password: '789' }
+];
+const usersTimeline = [
+	{ username: 'Estefany', timeline: 'Me encanta Javascript!' },
+	{ username: 'Oscar', timeline: 'Developer es lo mejor!' },
+	{ username: 'Mariana', timeline: 'A mi me gusta más el café que el té' },
+	{ username: 'Andres', timeline: 'Yo hoy no quiero trabajar' }
+];
+
+var botonEnviar = document.getElementById('botonEnviar');
+botonEnviar.addEventListener('click', ejecutar);
+
+function ejecutar() {
+	username();
+	password();
+	print();
+}
+
+function username() {
+	var username = document.getElementById('username').value;
+	return username;
+}
+function password() {
+	var password = document.getElementById('password').value;
+	return Number(password);
+}
+console.log(recibir());
+
+function print() {
+	if (username() === '' || password() === 0 ||'' ) {
+		document.querySelector('#text').innerHTML = `
+    <h1 class="text-red-600">Ingresa tu Usuario y Contraseña completos</h1>
+    `;
+	} else if ((password() !== Number(password())) === true) {
+		document.querySelector('#text').innerHTML = `
+        <h1 class="text-red-600">Solo se pueden ingresar numeros en la Contraseña</h1>
+        `;
+	} else if (username() === username() && password() === password()) {
+		document.querySelector('#text').innerHTML = `
+    <h1 class="text-green-600">Tu username es ${username()}</h1>
+    <h1 class="text-green-600">Tu password es ${password()}</h1>
+    `;
+	}
+}
