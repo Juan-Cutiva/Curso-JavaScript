da104be6ea556c4fa99ecd98e297f1543ad17200
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-13 15:40:50 -0500

    Async and Await

---
+++ src/notas/asincronia/asyncANDawait.js
@@ -0,0 +1,49 @@
+// function fetchData(){
+//     fetch('https://rickandmortyapi.com/api/character')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error('Error:', error));
+// }
+// fetchData();
+
+// async function fetchData(){
+//     try{
+//         let response = await fetch('https://rickandmortyapi.com/api/character');
+//         let data = await response.json();
+//         let morty = data.results[1].name
+//         console.log(data);
+//         return resultadoMorty = morty
+//     }catch(error){
+//         console.error('Error:', error);
+//     }
+// }
+// fetchData()
+let resultadoNombres = null
+async function fetchData(){
+    try{
+        let response = await fetch('https://rickandmortyapi.com/api/character');
+        let data = await response.json();
+        const element = data.results;
+        for (let i = 0; i < element.length; i++) {
+            let names = element[i].name;
+            resultadoNombres = names
+        }
+    }catch(error){
+        console.error('Error:', error);
+    }
+}
+fetchData()
+
+const promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		if (resultadoNombres === String(resultadoNombres)) {
+			resolve(`La operación se realizó con éxito, Se recupero los nombres de la api`);
+		} else {
+			reject('La operación no se pudo realizar');
+		}
+	}, 1000);
+});
+
+promise
+    .then((message) => console.log(message))
+    .catch((error) => console.error(error));
